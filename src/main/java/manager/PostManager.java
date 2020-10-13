a65c8fe40aa22551da908b271b34d90390b7c07c
Aidar <Agsafiullin@gmail.com>
2020-10-13 19:17:01 +0300

    fixed

--- src/main/java/manager/PostManager.java
+++ src/main/java/manager/PostManager.java
@@ -1,11 +1,16 @@
 package manager;
 
+import com.sun.org.apache.xpath.internal.objects.XBoolean;
 import domain.Post;
 
 public class PostManager {
     private Post[] posts;
 
-    public Post[] getPosts() {
+    public Post[] search(int authorID, String domain, String query, Boolean authorOnly, int count, int offset) {
         return null;
+    }
+
+    public void delete(int authorId, int postId) {
+
     }
 }
