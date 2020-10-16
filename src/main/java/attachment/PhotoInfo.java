f5be774d673412335482529ae06fae8f866be743
Aidar <Agsafiullin@gmail.com>
2020-10-16 22:03:32 +0300

    added attachment

---
+++ src/main/java/attachment/PhotoInfo.java
@@ -0,0 +1,14 @@
+package attachment;
+
+public class PhotoInfo {
+    private int photoId;
+    private int albumId;
+    private int ownerId;
+    private String text;
+    private int date;
+    private int width;
+    private int height;
+    private int countLike;
+
+
+}
---
+++ src/main/java/attachment/Video.java
@@ -0,0 +1,17 @@
+package attachment;
+
+public class Video {
+    private int videoId;
+    private int ownerId;
+    private String title;
+    private String description;
+    private int duration;
+    private int date;
+    private int adding_date;
+    private int local_view;
+    private int comments;
+
+
+
+
+}
